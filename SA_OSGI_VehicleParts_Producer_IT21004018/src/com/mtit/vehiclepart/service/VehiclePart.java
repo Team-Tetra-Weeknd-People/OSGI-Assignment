eee9e8b4fd2b148cf12c3d09fa5649d5825772b2
Randula Mawaththta <randula98@gmail.com>
2023-03-12 03:49:34 +0530

    Vehicle Parts Section Added

---
+++ SA_OSGI_VehicleParts_Producer_IT21004018/src/com/mtit/vehiclepart/service/VehiclePart.java
@@ -0,0 +1,28 @@
+package com.mtit.vehiclepart.service;
+
+public abstract class VehiclePart {
+	
+	protected String name;
+	protected double price;
+	
+	public VehiclePart(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
